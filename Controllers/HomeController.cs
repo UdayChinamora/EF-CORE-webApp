f758383eb980376de60852098d8b8c138c2c8900
UdayChinamora <uchinamhora@gmail.com>
2023-01-26 23:39:11 -0600

    Movies List Updated

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using MovieList.Models;
+
+using System.Diagnostics;
+
+namespace MovieList.Controllers
+{
+    public class HomeController : Controller
+    {
+       public MovieContext context { get; set; }
+
+        public HomeController (MovieContext ctx) => context = ctx;
+
+        public IActionResult Index()
+        {
+            var movies =context.Movies.OrderBy(m=> m.Name).ToList();
+           
+            
+            return View(movies);
+        }
+
+       
+
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace MovieList.Models
+{
+    public class Movie
+    {
+        //EF Core will configure the database to generate this value
+        public int MovieId { get; set; }
+
+        [Required(ErrorMessage = "Please enter name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter Year")]
+        [Range(1889, 2999, ErrorMessage = "Year must be after 1889.")]
+        public int? Year { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter Rating")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int? Rating { get; set; }
+
+
+
+      
+
+    }
+}
